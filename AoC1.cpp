6ab1cd262f435d7d68cdd94e534f64263b685e6d
M G. S <101915671+mintgumshoe@users.noreply.github.com>
2023-12-28 17:09:46 -0600

    Day 1 Solution
    I made this mess instead of studying for my finals.

---
+++ AoC1.cpp
@@ -0,0 +1,73 @@
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+unordered_map<string, char> numberMap = {
+    {"one", '1'},
+    {"two", '2'},
+    {"three", '3'},
+    {"four", '4'},
+    {"five", '5'},
+    {"six", '6'},
+    {"seven", '7'},
+    {"eight", '8'},
+    {"nine", '9'}
+};
+
+char findFirstNumber(string input) {
+    for (int i = 0; i < input.length(); ++i) {
+        if (isDigit(input[i])) {
+            return input[i];
+        }
+        string substring = "";
+        for (int j = i; j < input.length(); ++j) {
+            substring += input[j];
+            if (numberMap.find(substring) != numberMap.end()) {
+                return numberMap[substring];
+            }
+        }
+    }
+    return '0'; // indicates no number found
+}
+
+char findLastNumber(string input) {
+  for (int i = input.length() - 1; i >= 0; --i) {
+        if (isdigit(input[i])) {
+            return input[i];
+        }
+        string substring = "";
+        for (int j = i; j >= 0; --j) {
+            substring = input[j] + substring;
+            if (numberMap.find(substring) != numberMap.end()) {
+                return numberMap[substring];
+            }
+        }
+    }
+    return '0'; // indicates no number found
+}
+
+string sumNums(char firstNum, char lastNum) { 
+  string first(1, firstNum);
+  string last(1, lastNum);
+  return first + last; 
+}
+
+int main() {
+  // Open a file for reading
+  ifstream inputFile("example.csv");
+
+  // Read the file line by line
+  string line;
+  int bigSum = 0;
+  while (getline(inputFile, line)) {
+    bigSum += stoi(sumNums(findFirstNumber(line), findLastNumber(line)));
+  }
+
+  cout << "Sum: " << bigSum << endl; // Expect ANSWER! :D
+  
+  // Close the file
+  inputFile.close();
+
+}
